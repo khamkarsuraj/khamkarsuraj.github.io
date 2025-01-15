7a73555e39966212d384736ed44d9f76b7b7a02e
khamkarsuraj <112414255+khamkarsuraj@users.noreply.github.com>
2025-01-15 17:54:28 +0000

    Deploying to gh-pages from @ khamkarsuraj/khamkarsuraj.github.io@cb007c3ba82dee3b8e32df0e4278821817110039 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -474,6 +474,13 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/suraj-b-khamkar", "_blank");
         },
       },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/suraj_b_khamkar", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
